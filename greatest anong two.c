6f6c7141e5054a9f94a037f7423fd19ed7651365
Sandesha pokhrel <95642128+sandeshapokhrel@users.noreply.github.com>
2022-12-11 22:43:58 +0545

    Add files via upload

---
+++ greatest anong two.c
@@ -0,0 +1,15 @@
+//WAP in c to find the greatest among two number.
+#include<stdio.h>
+ int main()
+ {
+     int a,b;
+     printf("enter the value of a and b");
+     scanf("%d%d",&a,&b);
+     if(a>b)
+        printf("%d a is greatest",a);
+     else
+        printf("%d b is greatest",b);
+     return 0;
+
+
+ }
