a5b00e1a44062895ff9c174e36716dca48472ead
Sandesha pokhrel <95642128+sandeshapokhrel@users.noreply.github.com>
2022-12-11 22:51:53 +0545

    Add files via upload

---
+++ greatest among two using ternary.c
@@ -0,0 +1,12 @@
+//WAP in c to find the greatest among two number using ternary.
+#include<stdio.h>
+ int main()
+ {
+     int a,b;
+     printf("enter the value of a and b");
+     scanf("%d%d",&a,&b);
+     (a>b)? printf("%d a is greatest",a): printf("%d b is greatest",b);
+     return 0;
+
+
+ }
