e50dcaf139830e42c5d59ab13a1557d857634c8a
Sandesha pokhrel <95642128+sandeshapokhrel@users.noreply.github.com>
2022-12-11 22:04:29 +0545

    Add files via upload

---
+++ structure of a c.c
@@ -0,0 +1,19 @@
+//Documentation section(Short info of program & other info)
+//WAP to print hello world 11
+//header file/link inclusion section
+   #include<stdio.h>
+   #include<conio.h>
+//definition section
+   #define b 6
+//global declaration section
+//main function
+    int main()
+{
+
+//declaration part/local declaration
+    int a=5;
+//executable part
+   printf("hello world %d",a+b);
+   return 0;
+//sub program section(user define)
+}
