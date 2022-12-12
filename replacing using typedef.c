51cf680c0afdda4944de372914778b58973bbd93
Sandesha pokhrel <95642128+sandeshapokhrel@users.noreply.github.com>
2022-12-12 19:57:52 +0545

    Add files via upload

---
+++ replacing using typedef.c
@@ -0,0 +1,10 @@
+//divide a/b where a int and b float and replace a by your name and b by your surname.
+#include<stdio.h>
+ typedef int name ;
+ typedef float surname;
+ int main()
+{  name a=5;
+   surname b=5;
+   printf("%f the division is ",a/b);
+   return 0;
+}
